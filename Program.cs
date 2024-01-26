17d6d200829c64e4d6e4a5a533848e6fd997b61e
Vahdet Savcı <126013616+vahdetsavci@users.noreply.github.com>
2024-01-26 14:23:11 +0300

    app is done

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        PL pl = new PL();
+
+        do
+            pl.KullaniciKarsila(); 
+        while (Sistem.DevamEt());
     }
 }
